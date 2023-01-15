00048237f4648c11b0eb20af6f5e44b880ce3282
Aleksandr <aleksandr.evlanicov@gmail.com>
2023-01-15 14:23:22 +0300

    Dz2

---
+++ Dz2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+static int SumNaturalNumbers(int m, int n)
+{
+
+if (m == n)
+{
+    return m;
+}
+else
+{
+    return m + SumNaturalNumbers(m + 1, n);
+}
+}
+Console.WriteLine(SumNaturalNumbers(m,n));
