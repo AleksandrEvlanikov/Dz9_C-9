08e15144a860a67c9b46f079518289b68012ac86
Aleksandr <aleksandr.evlanicov@gmail.com>
2023-01-15 14:23:32 +0300

    Dz3

---
+++ Dz3/Program.cs
@@ -0,0 +1,22 @@
+﻿
+    int m = 2;
+    int n = 3;
+        
+     int A(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0)
+        {
+            return A(m - 1, 1);
+        }
+        else
+        {
+            return A(m - 1, A(m, n - 1));
+        }
+    }
+     Console.WriteLine(A(m, n));
+    
+
