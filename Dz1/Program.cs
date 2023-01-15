fe58de9f12cd3b578b36ef1923317f8878d7c03a
Aleksandr <aleksandr.evlanicov@gmail.com>
2023-01-15 14:23:14 +0300

    Dz1

---
+++ Dz1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void printNumbers( int n)
+{
+    if (n == 0)
+        return;
+    Console.WriteLine(n);
+    printNumbers(n-1);
+}
+printNumbers(n);
+ 
+
+    
